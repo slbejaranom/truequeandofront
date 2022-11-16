c18956f3bf530a4059e88b251668b777ae53c678
Sebastian Bejarano <slbejaranom@gmail.com>
2022-11-15 19:57:11 -0500

    Added principal page files, missing fillable forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from './components/principal/principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: PrincipalComponent,
+    children: [
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { MenuComponent } from './components/principal/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
