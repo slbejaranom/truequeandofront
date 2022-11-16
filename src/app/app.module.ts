ab504f4a319c3c1b19ace5a5826ae48d29e47291
Sebastian Bejarano <slbejaranom@gmail.com>
2022-11-16 00:06:38 -0500

    Login form done, missing form validations and submitting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MenuComponent } from './components/principal/menu/menu.component';
 import { IntroComponent } from './components/principal/intro/intro.component';
 import { LoginComponent } from './components/principal/login/login.component';
 import { RegisterComponent } from './components/principal/register/register.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { RegisterComponent } from './components/principal/register/register.comp
     RegisterComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
