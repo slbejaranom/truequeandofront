30148b6ba760c0690a6aaf3c3f97d9f7c70263f9
Sebastian Bejarano <slbejaranom@gmail.com>
2022-11-15 21:03:57 -0500

    Building intro page, menu is done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './components/principal/principal.component';
 import { MenuComponent } from './components/principal/menu/menu.component';
+import { IntroComponent } from './components/principal/intro/intro.component';
+import { LoginComponent } from './components/principal/login/login.component';
+import { RegisterComponent } from './components/principal/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrincipalComponent,
-    MenuComponent
+    MenuComponent,
+    IntroComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
