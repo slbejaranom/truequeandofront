ab16baf0e732ef2e62c28bbfb09cb056c15d58dc
Sebastian Bejarano <slbejaranom@gmail.com>
2022-11-16 22:10:25 -0500

    Pagina de intro casi finalizada

---
+++ src/app/components/principal/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private router : Router) { }
+
+  ngOnInit(): void {
+  }
+
+  onsubmit(loginForm : NgForm){
+
+  }
+
+  backToMain(){
+    this.router.navigateByUrl("/");
+  }
+
+}
