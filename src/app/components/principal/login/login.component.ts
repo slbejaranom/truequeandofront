d1a05d7cfd75f26d7a55899bba6c41cf35cfe38b
Sebastian Bejarano <slbejaranom@gmail.com>
2022-11-20 01:55:59 -0500

    Login finalizado

--- src/app/components/principal/login/login.component.ts
+++ src/app/components/principal/login/login.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -10,6 +11,14 @@ import { Usuario } from '../../../domain/usuario';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+
+  validUser : boolean = true;
+  validPassword : boolean = true;
+  serviceError : boolean = false;
+  error : Error = {
+    name:"",
+    message: ""
+  };
 
   constructor(private router : Router, private authService : AuthService) { }
 
@@ -28,17 +37,23 @@ export class LoginComponent implements OnInit {
         complete: () => {
           //ToDo: Login session logic
         },
-        error: () => {
-
+        error: (err : HttpErrorResponse) => {
+          this.serviceError = true;
+          this.error = this.authService.handleError(err);
         }
       });
     }
     else{
-
+      this.verifyLoginErrors(loginForm);
     }
   }
 
   backToMain(){
     this.router.navigateByUrl("/");
+  }
+
+  verifyLoginErrors(loginForm : NgForm){
+    this.validPassword = loginForm.controls["password"]?.valid;
+    this.validUser = loginForm.controls["username"]?.valid;
   }
 }
