2e4d5fd9555217b858b875027d7220220da88d3d
Sebastian Bejarano <slbejaranom@gmail.com>
2022-12-06 20:23:15 -0500

    Cambios de Kathe

--- src/app/components/home/landing/lista-objetos/lista-objetos.component.ts
+++ src/app/components/home/landing/lista-objetos/lista-objetos.component.ts
@@ -20,6 +20,42 @@ export class ListaObjetosComponent implements OnInit {
       "peso": 10,
       "valor": 10000,
       "urlImagen": "https://www.cocinayvino.com/wp-content/uploads/2016/09/14403475_l-696x462.jpg"
+    },
+    {
+      "id": 0,
+      "altura": 10,
+      "anchura": 10,
+      "longitud": 10,
+      "categoria": 0,
+      "estado": true,
+      "nombre": "Pepita de ahuyama",
+      "peso": 10,
+      "valor": 10000,
+      "urlImagen": "https://www.cocinayvino.com/wp-content/uploads/2016/09/14403475_l-696x462.jpg"
+    },
+    {
+      "id": 0,
+      "altura": 10,
+      "anchura": 10,
+      "longitud": 10,
+      "categoria": 0,
+      "estado": true,
+      "nombre": "Pepita de ahuyama",
+      "peso": 10,
+      "valor": 10000,
+      "urlImagen": "https://www.cocinayvino.com/wp-content/uploads/2016/09/14403475_l-696x462.jpg"
+    },
+    {
+      "id": 0,
+      "altura": 10,
+      "anchura": 10,
+      "longitud": 10,
+      "categoria": 0,
+      "estado": true,
+      "nombre": "Pepita de ahuyama",
+      "peso": 10,
+      "valor": 10000,
+      "urlImagen": "https://www.cocinayvino.com/wp-content/uploads/2016/09/14403475_l-696x462.jpg"
     }
   ];
   numeroElementosParaMostrar : number = 4;
